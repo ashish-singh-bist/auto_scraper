bb05c649e2d407564d9fbd5520213e7c668e85e1
ashish-singh-bist <ashish.singh.bist@GMAIL.COM>
2018-08-20 16:26:08 +0530

    merge scrapper in laravel

---
+++ webapp/resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.myapp')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <!-- homepage input form -->
+            <div id="website-input-form" class="container">
+
+                <img width="142" height="142" src="img/ic_open_in_browser_black_48dp_2x.png" />
+
+                <form class="form-inline" method="post" id="search_site_form">
+                    <label for="file-upload" class="custom-file-upload" id="label-file-upload">
+                        Click to Upload File
+                    </label>
+                    <input id="file-upload" type="file" name="file-upload" />
+                    <button style="display:none;" id="submit-btn" type="submit">Go</button><br>
+                    <input id="file-analyze" type="checkbox" class="custom-file-upload" name="file-analyze" /> Analyze page traffic?
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
