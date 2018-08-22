9b86de9dcfed79ea64962e03254ddbf79d6b8867
ashish-singh-bist <ashish.singh.bist@GMAIL.COM>
2018-08-22 17:31:22 +0530

    add loader CSS

--- webapp/resources/views/vendor/adminlte/master.blade.php
+++ webapp/resources/views/vendor/adminlte/master.blade.php
@@ -37,7 +37,8 @@
 
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
+    <!-- <link href="css/jquery-ui.css" rel="stylesheet"> -->
+    <link href="css/from-the-page.css" rel="stylesheet">
     <!-- Styles -->
 
     <script src="config/config.js"></script>
