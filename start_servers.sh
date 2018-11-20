f818e9c8320a9ae94a9b2d33da60875a766537f2
Jitendra Kothiyal (Jeet) <jitendraworking@gmail.com>
2018-11-20 07:07:59 +0000

    added command line argument in start_servers.sh

--- start_servers.sh
+++ start_servers.sh
@@ -1,11 +1,21 @@
-#check and kill existing running node server if already running
-#echo 'Checking Port'
-#sudo kill $(sudo lsof -t -i:7001)
+#check and free port if already in use
+echo "Checking Port.."
+if lsof -Pi :$3 -sTCP:LISTEN -t >/dev/null ; then
+    echo "$3 port already in use, trying to free..."
+    sudo kill $(sudo lsof -t -i:$3)
+    echo "Now port $3 is available"
+fi
 
-echo 'Running node server in background...'
+if lsof -Pi :$2 -sTCP:LISTEN -t >/dev/null ; then
+    echo "$2 port already in use, trying to free..."
+    sudo kill $(sudo lsof -t -i:$2)
+    echo "Now port $2 is available"
+fi
+
+echo "Running node server in background..."
 node node_server/server.js &
-echo 'Node server started in background...'
+echo "Node server started in background..."
 
-echo 'Running web server...'
-php webapp/artisan serve
-echo 'Web server started...'
+echo "Running web server..."
+php webapp/artisan serve --host $1 --port $2
+echo "Web server started ..."
